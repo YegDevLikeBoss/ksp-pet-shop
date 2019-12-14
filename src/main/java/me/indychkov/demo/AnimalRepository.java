9fad96881f44b5ec22d1769fcd34e718f4202741
Ilya <ilya.dychkov@gmail.com>
2019-12-14 09:42:17 +0300

    Initial commit

---
+++ src/main/java/me/indychkov/demo/AnimalRepository.java
@@ -0,0 +1,14 @@
+package me.indychkov.demo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+public interface AnimalRepository extends JpaRepository<Animal, Integer> {
+
+    List<Animal> findAllByKind(@Param("kind") String kind);
+
+    List<Animal> findAllByNameContaining(@Param("name") String name);
+}
---
+++ src/test/java/me/indychkov/demo/CsaApplicationTests.java
@@ -0,0 +1,13 @@
+package me.indychkov.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
